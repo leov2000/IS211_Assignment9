64ec074f4c1ed274fcd868cf39cb8f572015304a
Leo <leovergarah@gmail.com>
2019-10-22 23:27:54 -0400

    (Added) touchdown method

--- football_stats.py
+++ football_stats.py
@@ -13,13 +13,15 @@ def merge_links(href_list, string):
 def split_link(link, pattern):
     return link.split(pattern)
 
+def get_touchdowns(soup):
+    return soup.find_all('a', text='Touchdowns')[0]['href']
+
 def main():
     nfl_link = 'http://www.cbssports.com/nfl/stats'
     soup = get_contents(nfl_link)
-    touchdown_link = soup.find_all('a', text='Touchdowns')[0]['href']
+    touchdown_link = get_touchdowns(soup)
     link_pattern = '/nfl/stats'
-    new_nfl_link = merge_links(split_link(nfl_link, link_pattern), touchdown_link)
-    print(new_nfl_link)
+    touchdown_link = merge_links(split_link(nfl_link, link_pattern), touchdown_link)
 
 if __name__ == '__main__':
     main()
