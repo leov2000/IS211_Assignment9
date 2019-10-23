fa6974872a01d391f9a8c27e148d45a63ab2aff1
Leo <leovergarah@gmail.com>
2019-10-22 23:37:25 -0400

    (Cleanup) variable names

--- football_stats.py
+++ football_stats.py
@@ -10,18 +10,18 @@ def get_contents(url):
 def merge_links(href_list, string):
     return ''.join(href_list) + string  
 
-def split_link(link, pattern):
-    return link.split(pattern)
+def split_link(url, pattern):
+    return url.split(pattern)
 
 def get_touchdowns(soup):
     return soup.find_all('a', text='Touchdowns')[0]['href']
 
 def main():
-    nfl_link = 'http://www.cbssports.com/nfl/stats'
-    soup = get_contents(nfl_link)
-    touchdown_link = get_touchdowns(soup)
+    nfl_url = 'http://www.cbssports.com/nfl/stats'
+    soup = get_contents(nfl_url)
+    touchdown = get_touchdowns(soup)
     link_pattern = '/nfl/stats'
-    touchdown_link = merge_links(split_link(nfl_link, link_pattern), touchdown_link)
+    touchdown_url = merge_links(split_link(nfl_url, link_pattern), touchdown)
 
 if __name__ == '__main__':
     main()
